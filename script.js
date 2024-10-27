a86d430e1c40c99ce21498729f7a12b5d9a98c86
TeamDarkBot <plkjool791@gmail.com>
2024-10-27 07:15:28 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+function toggleMenu() {
+  const navbar = document.getElementById('navbar');
+  if (navbar.style.display === 'flex') {
+    navbar.style.display = 'none';
+  } else {
+    navbar.style.display = 'flex';
+  }
+}
+
 function downloadCode() {
   const link = document.createElement('a');
   link.href = '/storage/emulated/Download/SpicydevSite.zip';  // Path to your file
